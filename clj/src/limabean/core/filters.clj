8269bfdcc74da76d622b117f9486c7dc5aa9df0e
Simon Guest <simon.guest@tesujimath.org>
2026-01-15 11:31:48 +1300

    Registry, with date filters taking strings

--- clj/src/limabean/core/filters.clj
+++ clj/src/limabean/core/filters.clj
@@ -4,23 +4,27 @@
 
 (defn date<
   "Predicate for :date field to be < begin-date, or false if no date field"
-  [end-date]
-  #(let [date (:date %)] (and date (jt/before? date end-date))))
+  [end-date-str]
+  (let [end-date (jt/local-date end-date-str)]
+    #(let [date (:date %)] (and date (jt/before? date end-date)))))
 
 (defn date<=
   "Predicate for :date field to be <= end-date, or false if no date field"
-  [end-date]
-  #(let [date (:date %)] (and date (jt/not-after? date end-date))))
+  [end-date-str]
+  (let [end-date (jt/local-date end-date-str)]
+    #(let [date (:date %)] (and date (jt/not-after? date end-date)))))
 
 (defn date>
   "Predicate for :date field to be > begin-date, or false if no date field"
-  [begin-date]
-  #(let [date (:date %)] (and date (jt/after? date begin-date))))
+  [begin-date-str]
+  (let [begin-date (jt/local-date begin-date-str)]
+    #(let [date (:date %)] (and date (jt/after? date begin-date)))))
 
 (defn date>=
   "Predicate for :date field to be >= begin-date, or false if no date field"
-  [begin-date]
-  #(let [date (:date %)] (and date (jt/not-before? date begin-date))))
+  [begin-date-str]
+  (let [begin-date (jt/local-date begin-date-str)]
+    #(let [date (:date %)] (and date (jt/not-before? date begin-date)))))
 
 (defn acc=
   "Predicate for :acc field to be equal to acc, or false if no acc field"
