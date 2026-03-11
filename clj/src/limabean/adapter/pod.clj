a6fc8b55652e449f254e43722c080eeb451804dc
Simon Guest <simon.guest@tesujimath.org>
2026-03-11 13:33:10 +1300

    Keywords for booking values

--- clj/src/limabean/adapter/pod.clj
+++ clj/src/limabean/adapter/pod.clj
@@ -33,12 +33,13 @@
 
 (defn convert-values
   [data]
-  (walk/postwalk (fn [x]
-                   (cond-> x
-                     (and (map? x) (contains? x :date)) (update :date
-                                                                jt/local-date)
-                     (and (map? x) (contains? x :dct)) (update :dct keyword)))
-                 data))
+  (walk/postwalk
+    (fn [x]
+      (cond-> x
+        (and (map? x) (contains? x :date)) (update :date jt/local-date)
+        (and (map? x) (contains? x :dct)) (update :dct keyword)
+        (and (map? x) (contains? x :booking)) (update :booking keyword)))
+    data))
 
 (defn read-msg
   "Read and decode a response, using BigDecimal for all numbers, and converting values as appropriate"
