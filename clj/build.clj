432bd11e508081c0c6a718bc2d2178a13fda8e04
Simon Guest <simon.guest@tesujimath.org>
2026-03-23 13:27:34 +1300

    Use rebel-readline 0.1.7 from Clojars
    Avoids the awkwardness of installing its dependencies.

--- clj/build.clj
+++ clj/build.clj
@@ -32,20 +32,6 @@
 
 (def basis (b/create-basis {:project "deps.edn"}))
 
-;; TODO remove this and vendoring below once rebel readline 0.1.7 available on
-;; Clojars
-;; but for now we need it because transitive dependencies aren't
-;; loaded via a git co-ordinate
-(def basis-with-github-deps
-  (b/create-basis
-    {:project "deps.edn",
-     :extra {:deps {'compliment/compliment {:mvn/version "0.6.0"},
-                    'dev.weavejester/cljfmt {:mvn/version "0.13.0"},
-                    'org.jline/jline-reader {:mvn/version "3.30.0"},
-                    'org.jline/jline-terminal {:mvn/version "3.30.0"},
-                    'org.jline/jline-terminal-jni {:mvn/version "3.30.0"}}}}))
-
-
 (defn- pom-template
   [version]
   [[:description "A new implementation of Beancount using Rust and Clojure."]
@@ -82,7 +68,7 @@
                  :class-dir class-dir
                  :lib lib
                  :version version
-                 :basis basis-with-github-deps
+                 :basis basis
                  :src-dirs ["src"]
                  :src-pom :none
                  :pom-data (pom-template version)))
@@ -108,11 +94,6 @@
         opts (jar-opts opts)]
     (println "\nCopying source...")
     (b/copy-dir {:src-dirs ["resources" "src"], :target-dir class-dir})
-    ;; TODO remove this once rebel-readline 0.1.7 available on Clojars
-    (println "\nVendoring rebel-readline")
-    (b/compile-clj (assoc opts
-                     :basis basis
-                     :ns-compile '[rebel-readline.clojure.main]))
     (println "\nBuilding jar" (:jar-file opts))
     (b/jar opts)
     opts))
@@ -123,7 +104,7 @@
     :class-dir class-dir
     :uber-file uber-file
     :main main
-    :basis basis-with-github-deps
+    :basis basis
     :manifest {"Implementation-Version" version, "Add-Modules" "java.sql"}))
 
 (defn uber
